28708bac12c654fb53c50a4edb0b3f8d1e1f8055
gsuiz <156917760+gsuiz@users.noreply.github.com>
2024-09-11 07:11:32 -0300

    criei o script

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const nome = prompt("Qual é seu nome?")
+
+if(nome === "Kleydson"){
+    console.log("Muito bem vindo senhor!")
+} else if(nome === "kleydson"){
+    console.log("Você é um trapaceiro!")
+} else if(nome === "Guilherme"){
+    console.log("Muito bem vindo senhor!")
+} else if(nome === "guilherme"){
+    console.log("Você é um trapaceiro")
+} else {
+    console.log("Ele está ocupado")
+}
